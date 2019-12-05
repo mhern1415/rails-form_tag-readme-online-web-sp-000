93e90e9c0b52b3d156d3d1e03346aed0c48f442c
Mark Hernandez <m.hern1415@gmail.com>
2019-12-05 00:40:53 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,5 +4,11 @@ describe 'new post' do
   it 'ensures that the form route works with the /new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
+    
+  it 'renders HTML in the /new template' do
+    visit new_post_path
+    expect(page).to have_content('Post Form')
+  end
+    
   end
 end
